1249fab58bed020345315a358d0a23952b264125
KornSiwat <ponpued@gmail.com>
2019-05-27 17:41:08 +0700

    refactored ScoreBoardScene

--- scenes/ScoreBoardScene.py
+++ scenes/ScoreBoardScene.py
@@ -1,30 +1,26 @@
 import arcade
 
 class ScoreBoardScene():
-    def __init__(self, width, height, scoreManager, assetManager=0):
+    def __init__(self, width, height, scoreManager):
         self._width = width
         self._height = height
 
-        self.assetManager = assetManager
+        self._scoreManager = scoreManager
 
+        self.setup_assets()
+        self.setup_table()
+
+    def setup_assets(self):
         self._grayBackground = arcade.load_texture("images/result.png")
         self._scoreboardBackground = arcade.load_texture('images/scoreboardbg.png')
         self._background_width = self._width-250
         self._background_height = self._height-150
-
-
-        self._scoreManager = scoreManager
-
-        self._scoreboardPic = arcade.load_texture('images/scoreboard.png')
-
         self._header = arcade.Sprite()
         self._header.append_texture(arcade.load_texture('images/scoreboard.png'))
         self._header.set_texture(0)
 
-        self._header_text = []
-
     def draw(self):
-        ''' draw scoreboard scene element '''
+        ''' draw scoreboard scene elements '''
 
         self.draw_background()
         self.draw_header()
@@ -41,21 +37,27 @@ class ScoreBoardScene():
         self._header.draw()
 
     def draw_table(self):
-        self.draw_table_header()
-        self.draw_table_body()
+        for row in self._table_data:
+            self.draw_text(row[0], row[1], row[2])
 
-    def draw_table_header(self):
-        self.draw_text(x=-280, y=100, word='No.')
-        self.draw_text(x=-200, y=100, word='Words')
-        self.draw_text(x=-75, y=100, word='Word Per Minute')
-        self.draw_text(x=170, y=100, word='Total Time')
+    def setup_table(self):
+        self.config_table_header()
+        self.config_table_body()
 
-    def draw_table_body(self):
+    def config_table_header(self):
+        self._table_data = [
+            [-280, 100, 'No.'],
+            [-200, 100, 'Words'],
+            [-75, 100, 'Word Per Minute'],
+            [170, 100, 'Total Time']
+        ]
+
+    def config_table_body(self):
         for no, matchStat in enumerate(self._scoreManager.get_latest_five(),1):
-            self.draw_text(x=-270, y= 90-(30 * no), word=str(no))
-            self.draw_text(x=-180, y= 90-(30 * no), word=f'{matchStat.wordAmount:02.2f}')
-            self.draw_text(x=0, y= 90-(30 * no), word=f'{matchStat.wordPerMinute}')
-            self.draw_text(x=200, y= 90-(30 * no), word=f'{matchStat.totalTime}')
+            self._table_data.append([-270, 90-(30 * no), str(no)])
+            self._table_data.append([-180, 90-(30 * no), f'{matchStat.wordAmount:02.2f}'])
+            self._table_data.append([0, 90-(30 * no), f'{matchStat.wordPerMinute}'])
+            self._table_data.append([200, 90-(30 * no), f'{matchStat.totalTime}'])
 
     def draw_high_score(self):
         self.draw_text(x=-320, y=-120, word=f'Local Best Time: {self._scoreManager.get_best()[1]:.2f} s')
